d71fecbadd1bbb687a4be5e8a9abf7811f21cfff
Ali  Chishti <alimuhammad@Alis-MacBook-Air-3.local>
2022-06-28 19:17:27 +0500

    feat: add login error:

--- frontend/src/app/services/error-handler.service.ts
+++ frontend/src/app/services/error-handler.service.ts
@@ -7,6 +7,11 @@ import { Observable, of } from 'rxjs';
 export class ErrorHandlerService {
   handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
+      const loginError: HTMLElement | null =
+        document.getElementById('login-error');
+      if (loginError) {
+        loginError.textContent = "Username, Email or Password is invalid!";
+      }
       console.log(`${operation} failed: ${error.msg}`);
       return of(result as T);
     };
